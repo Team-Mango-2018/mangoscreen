e4e11eec538dc64aa5d6f7fd7add560a0cb62d76
Penuel Maypa <penmayp@gmail.com>
2018-12-15 17:14:51 +0000

    post-change -- if statement on the php so it database config to switch when running local and running penuel.site

--- page/includes/dbh.inc.php
+++ page/includes/dbh.inc.php
@@ -1,8 +1,22 @@
 <?php
 
-$servername = "penmayp50734.ipagemysql.com";
-$username = "mango_db_access";
-$password = "#-mang-pass-2819-*";
+$host= gethostname();
+$ip = gethostbyname($host);
+$db_server =  substr($ip , 0, 7);  // returns the first 7 character
+
+if ( $db_server == '192.168' ) {
+   $servername = 'localhost';
+   $username = 'root' ;
+   $password = '';
+   $message_1 = "<p> The server is a local server </p>";
+}else{
+   $servername = "penmayp50734.ipagemysql.com";
+   $username = "mango_db_access" ;
+   $password = "#-mang-pass-2819-*" ;
+   $message_1 = "<p> The server is not local </p>";
+}
+
+
 $dbName = "mango_screen";
 
 $conn = mysqli_connect($servername, $username, $password, $dbName);
